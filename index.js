dc0a31d770cb5385e6f4481b5727507ea4555cf7
OssiB <ossi.bister@gmail.com>
2019-12-05 10:23:20 +0200

    fix

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-app.use(morgan('tiny'),{
-   
-})
+const cors = require('cors')
+
+app.use(cors())
 app.use(bodyParser.json())
+morgan.token('person', function getPerson(req,res) { return JSON.stringify (req.body) })
+
+
+const assignPerson = (req,res,next)  =>  {
+  if(req.method === 'POST'){
+    req.person = req.body
+  }
+  else {
+    req.person = null
+  }
+  next()
+}
+/*app.use(assignPerson )
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person '))*/
+
 
 let persons = [
 
@@ -36,6 +52,7 @@ let persons = [
 
 
 app.get('/api/persons', (req, res) => {
+  
   res.json(persons)
 })
 app.get('/api/persons/:id', (request, response) => {
@@ -47,26 +64,33 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
-app.delete('/persons/:id', (request, response) => {
+app.delete('api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
+  console.log(id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
 
-app.post('/api/persons/', (request, response) => {
-  const body = req.body 
+app.post('/api/persons/', (req, res) => {
+  const body = req.body
   if (!body.name || !body.number) {
     return res.status(404).json({
       error: 'name or number is missing'
     })
   }
   const person = persons.find(person => person.name === body.name)
-  if(person){
+  if (person) {
     return res.status(404).json({
       error: 'name is already in use'
     })
   }
+  const newPerson = {
+    name: body.name,
+    number: body.number
+  }
+  persons.concat(newPerson)
+  res.json(newPerson)
 })
 app.get('/info', (request, respose) => {
   const info = `<p>Phonebook has info for ${persons.length} people</p>
