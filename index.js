58c1388c0f9251e9ea093e19e6362e16d3c416ad
OssiB <ossi.bister@gmail.com>
2019-12-06 23:44:25 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+require('dotenv').config()
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
 const bodyParser = require('body-parser')
+const Contact = require('./models/contact')
 const morgan = require('morgan')
-
+app.use(bodyParser.json())
 const cors = require('cors')
 
 app.use(cors())
-app.use(bodyParser.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.static('build'))
-morgan.token('person', function getPerson(req,res) { return JSON.stringify (req.body) })
+app.use(requestLogger)
+morgan.token('person', function getPerson(req, res) { return JSON.stringify(req.body) })
 
 
-const assignPerson = (req,res,next)  =>  {
-  if(req.method === 'POST'){
+const assignPerson = (req, res, next) => {
+  if (req.method === 'POST') {
     req.person = req.body
   }
   else {
@@ -25,84 +38,66 @@ const assignPerson = (req,res,next)  =>  {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person '))*/
 
 
-let persons = [
-
-  {
-    "name": "Martti Tienari",
-    "number": "040-123456",
-    "id": 2
-  },
-  {
-    "name": "Arto Järvinen",
-    "number": "040-123456",
-    "id": 3
-  },
-  {
-    "name": "Lea Kutvonen",
-    "number": "040-123456",
-    "id": 4
-  },
-  {
-    "name": "Ossi Bister",
-    "number": "040 77 0532",
-    "id": 5
-  }
-
-]
-
-
 
 app.get('/api/persons', (req, res) => {
-  
-  res.json(persons)
+  Contact.find({}).then(contacts => {
+    res.json(contacts.map(contact => contact.toJSON()))
+  });
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if (contact) {
+        response.json(contact.toJSON())
+      } else {
+        response.status(204).end()
+      }
+    })
+    .catch(error => next(error))
+})
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const contact = {
+    number: body.number,
+    name: body.name,
   }
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then(updatedNote => {
+      response.json(updatedContact.toJSON())
+    })
+    .catch(error => next(error))
 })
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  console.log(id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons/', (req, res) => {
-  const body = req.body
-  if (!body.name || !body.number) {
-    return res.status(404).json({
-      error: 'name or number is missing'
-    })
-  }
-  const person = persons.find(person => person.name === body.name)
-  if (person) {
-    return res.status(404).json({
-      error: 'name is already in use'
-    })
-  }
-  const newPerson = {
-    name: body.name,
-    number: body.number
-  }
-  persons.concat(newPerson)
-  res.json(newPerson)
-})
-app.get('/info', (request, respose) => {
-  const info = `<p>Phonebook has info for ${persons.length} people</p>
-                \n 
-                <p>${new Date()}</p>`
+  const body = request.body
 
-  respose.send(info)
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const contact = new Contact({
+    number: body.number,
+    important: body.name
+  })
+
+  contact.save().then(savedContact => {
+    response.json(savedContact.toJSON())
+  })
 })
+
+
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- mongo.js
+++ mongo.js
@@ -1,32 +1,44 @@
 const mongoose = require('mongoose')
 
-if ( process.argv.length<3 ) {
-  console.log('give password as argument')
+if (process.argv.length < 3) {
+  console.log('give a password at least as  argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
 
+
 const url =
-  `mongodb+srv://puhelin_3b:${password}@cluster0-meam4.mongodb.net/test?retryWrites=true&w=majority`
+  `mongodb+srv://puhelin_3b:${password}@cluster0-meam4.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
 console.log(url)
 mongoose.connect(url, { useNewUrlParser: true })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String
+
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Contact = mongoose.model('Contact', contactSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
 
-note.save().then(response => {
-  console.log('note saved!');
-  mongoose.connection.close();
-})
+if (process.argv.length == 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const contact = new Contact({
+    name: name,
+    number: number
+  })
+  contact.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  })
+}
+else {
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact)
+    })
+    mongoose.connection.close()
+  })
+}
